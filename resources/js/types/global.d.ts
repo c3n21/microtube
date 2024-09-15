a64c93c6f433fd412512e071b4f0681d77355acb
c3n21 <chenzhifan9@gmail.com>
2024-09-15 22:12:13 +0200

    fixed ziggy type issue

--- resources/js/types/global.d.ts
+++ resources/js/types/global.d.ts
@@ -1,7 +1,23 @@
-import { PageProps as InertiaPageProps } from '@inertiajs/core';
-import { AxiosInstance } from 'axios';
-import { route as ziggyRoute } from 'ziggy-js';
-import { PageProps as AppPageProps } from './';
+import { PageProps as InertiaPageProps } from "@inertiajs/core";
+import { AxiosInstance } from "axios";
+import { route as ziggyRoute } from "ziggy-js";
+import { Config } from "../../../vendor/tightenco/ziggy";
+
+export interface User {
+    id: number;
+    name: string;
+    email: string;
+    email_verified_at?: string;
+}
+
+export type AppPageProps<
+    T extends Record<string, unknown> = Record<string, unknown>,
+> = T & {
+    auth: {
+        user: User;
+    };
+    ziggy: Config & { location: string };
+};
 
 declare global {
     interface Window {
@@ -11,12 +27,12 @@ declare global {
     var route: typeof ziggyRoute;
 }
 
-declare module 'vue' {
+declare module "vue" {
     interface ComponentCustomProperties {
         route: typeof ziggyRoute;
     }
 }
 
-declare module '@inertiajs/core' {
-    interface PageProps extends InertiaPageProps, AppPageProps {}
+declare module "@inertiajs/core" {
+    export interface PageProps extends InertiaPageProps, AppPageProps {}
 }
