14bb9414c8ca4168d994c22de419857b3e78043b
c3n21 <chenzhifan9@gmail.com>
2024-09-15 16:47:51 +0200

    feat(AuthenticatedLayout): hide dropdown if user is not authenticated

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import VideoCard from "@/Components/VideoCard.vue";
+import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout.vue";
 import type { Video } from "@/types/graphql";
 import { Head } from "@inertiajs/vue3";
 
@@ -10,9 +11,11 @@ defineProps<{
 
 <template>
     <Head title="Microtube" />
-    <div class="bg-gray-50 text-black/50 dark:bg-black dark:text-white/50">
-        <div v-for="initialVideo in initialVideos" :key="initialVideo.id">
-            <VideoCard :video="initialVideo" />
+    <AuthenticatedLayout>
+        <div class="bg-gray-50 text-black/50 dark:bg-black dark:text-white/50">
+            <div v-for="initialVideo in initialVideos" :key="initialVideo.id">
+                <VideoCard :video="initialVideo" />
+            </div>
         </div>
-    </div>
+    </AuthenticatedLayout>
 </template>
