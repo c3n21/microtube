e99ed419fff67eb1f57fbfb3ee944e4365c1d10e
c3n21 <chenzhifan9@gmail.com>
2024-10-02 14:04:33 +0200

    AddWatermarkToVideo working

---
+++ app/Console/Commands/AddWatermarkToVideo.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use ProtoneMedia\LaravelFFMpeg\Filters\WatermarkFactory;
+use ProtoneMedia\LaravelFFMpeg\Support\FFMpeg;
+
+/**
+ * Add a watermark to the given video file.
+ *
+ * It's used for testing purposes.
+ *
+ * Caveats:
+ * - since this uses the LaravelFFMpeg package, it only works for files that are under the 'videos' disk.
+ */
+class AddWatermarkToVideo extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:add-watermark-to-video {filepath} {target}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Add a watermark to the given video file';
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        $filepath = $this->argument('filepath');
+        $target = $this->argument('target');
+
+        if (
+            !is_string($filepath)
+        ) {
+            $filepathType = gettype($filepath);
+            $this->error("The argument 'filepath' is expected to be 'string' but got '$filepathType' ($filepath)  does not exist.");
+            return 1;
+        }
+
+        if (
+            !is_string($target)
+        ) {
+            $targetType = gettype($target);
+            $this->error("The argument 'target' is expected to be 'string' but got '$targetType' ($target)  does not exist.");
+            return 1;
+        }
+
+        try {
+            self::addWatermarkToVideo($filepath, $target);
+
+            $this->info("Watermark added successfully to {$filepath}.");
+            return 0;
+        } catch (\Exception $e) {
+            $this->error("An error occurred: " . $e->getMessage());
+            return 1;
+        }
+    }
+
+    public static function addWatermarkToVideo(string $filepath, string $target): void
+    {
+        FFMpeg::fromDisk('videos')->open($filepath)
+            ->addWatermark(function (WatermarkFactory $watermarkFactory) {
+                return $watermarkFactory->fromDisk('public')
+                    ->open('microtube.png')
+                    ->resize(50, 50)
+                    ->right(25)
+                    ->bottom(25);
+            })
+            ->export()
+            ->toDisk('videos')
+            ->inFormat(new \FFMpeg\Format\Video\X264())
+            ->save($target);
+    }
+}
